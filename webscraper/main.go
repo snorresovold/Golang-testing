36de1c4b4a366131e6a2b8258b35f94c1ff885e1
Snorre Søvold <snorresovold@gmail.com>
2022-04-28 19:49:41 +0200

    updates

--- webscraper/main.go
+++ webscraper/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"log"
 	"os"
+
+	"github.com/gocolly/colly"
 )
 
 func main() {
@@ -14,5 +17,20 @@ func main() {
 		return
 	}
 	defer file.Close() // closes the file
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("vg.no"),
+	)
+
+	c.OnHTML("article article-extract article-width-full has-article-image has-full-width-image newsroom-vg skin-default", func(e *colly.HTMLElement) {
+
+		writer.Write([]string{
+			e.ChildText("h3"),
+			e.ChildText("a"),
+		})
+	})
 
 }
